19a62b0c9e07d509719a50c12ee8a28d11d749ad
Chris Brulak <chris.brulak@ama.ab.ca>
2013-02-14 16:11:04 -0700

    deleting old files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Plasma::Application.config.secret_token = 'e91d21a7751303ffb9feca410e62faf4fe3fcbb6d538b4d508b14501d1f1fa80076afabe4abe6464c9d5b4b984583420efb072242eaf41f0c14c82e55400a77d'
